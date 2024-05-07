bfaa47f9d1b254c9d99df9632ed2313d2933554e
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2024-05-08 02:32:06 +0300

    added errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import userRouter from './routes/users';
 import { createUser, login } from './controllers/users';
 import auth from './middlewares/auth';
 import { errorLogger, requestLogger } from './middlewares/logger';
+import { errorHandler } from './middlewares/error-handler';
 
 const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
@@ -33,13 +34,15 @@ app.use(requestLogger); // подключаю логер запросов
 app.post('/signin', login); // логин
 app.post('/signup', createUser); // регистрация
 
-app.use(auth); // авторизация
+app.use(auth); // мидлвар авторизации
 
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 app.use('/', notFoundRouter);
 
 app.use(errorLogger); // подключаю логер ошибок
+
+app.use(errorHandler); // подключаю мидлвар - централизованный обработчик ошибок
 
 // Подключаюсь к базе данных:
 const connect = async () => {
--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,5 +1,26 @@
 import { NextFunction, Request, Response } from "express";
+import { INTERNAL_SERVER_ERROR } from '../constants/constants';
 
-export default (err, req: Request, res: Response, next: NextFunction) => {
-  res.send({ message: err.message });
+interface ICustomError extends Error {
+  statusCode: number,
+  message: string,
+}
+
+export const errorHandler = (
+  err: ICustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { statusCode = INTERNAL_SERVER_ERROR, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === INTERNAL_SERVER_ERROR
+        ? 'The server encountered an error and could not complete your request'
+        : message,
+    });
 };
+
+export default { errorHandler };
